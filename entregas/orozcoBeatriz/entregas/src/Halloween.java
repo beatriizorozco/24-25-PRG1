fb827aa377b8bb3584dadc23024ce80388fe381f
Beatriz Orozco <beatrizorozcodelgado@gmail.com>
2026-03-08 19:47:12 +0100

    Cambio para que no haya Don't Repeat Yourself

--- entregas/orozcoBeatriz/entregas/src/Halloween.java
+++ entregas/orozcoBeatriz/entregas/src/Halloween.java
@@ -12,65 +12,38 @@ public class Halloween {
       final double P_CARAMELOS = 0.8;
       final int MAX_CARAMELOS_POR_CASA= 3;
       final int MIN_CARAMELOS_POR_CASA = 1;
-
-      int caramelosNino1 = 0,  caramelosNino2 = 0, caramelosNino3 = 0;
-    
-      boolean bolsaNino1 = false, bolsaNino2 = false, bolsaNino3 = false;
+        
+    int[] caramelos = {0, 0, 0};
+    boolean[] bolsaLlena = {false, false, false};
     
       int pisoActual = 0; 
       int casasVisitadas = 0; 
-      int numeroCaramelos;
-      double danCaramelos;
       
       do { 
         pisoActual++;
         System.out.println("\n=== Piso " + pisoActual + " ==="); 
+          
         for (int i = 1; i <= MAX_CASAS; i++) {
+            
           casasVisitadas++;
           System.out.println("\nVisitando casa " + i + " del piso " + pisoActual);
          
           double casaAbierta = Math.random();
+            
           if(casaAbierta <= P_ABIERTA) {
             System.out.println("¡La casa está abierta!");
             danCaramelos = Math.random();
-            
-            if(!bolsaNino1){
-              if(danCaramelos <= P_CARAMELOS){
-                numeroCaramelos = (int)(Math.random() * (MAX_CARAMELOS_POR_CASA - MIN_CARAMELOS_POR_CASA + 1)) + MIN_CARAMELOS_POR_CASA;
-                caramelosNino1 += numeroCaramelos;
-                System.out.println("Niño 1 recibió " + numeroCaramelos + " caramelos");      
-                if (caramelosNino1 >= MAX_CARAMELOS) { 
-                  bolsaNino1 = true;
-                  caramelosNino1 = MAX_CARAMELOS; 
-                  System.out.println("¡La bolsa del niño 1 está llena!"); 
-                }
-              } 
-            }
-                        
-            if(!bolsaNino2) {
-              if(danCaramelos <= P_CARAMELOS) {
-                numeroCaramelos = (int)(Math.random() * (MAX_CARAMELOS_POR_CASA - MIN_CARAMELOS_POR_CASA + 1)) + MIN_CARAMELOS_POR_CASA;
-                caramelosNino2 += numeroCaramelos;
-                System.out.println("Niño 2 recibió " + numeroCaramelos + " caramelos");          
-                if(caramelosNino2 >= MAX_CARAMELOS) { 
-                  bolsaNino2 = true;
-                  caramelosNino2 = MAX_CARAMELOS;
-                  System.out.println("¡La bolsa del niño 2 está llena!"); 
-                }
-              }
-            }
-                        
-            if(!bolsaNino3) {
-              if(danCaramelos <= P_CARAMELOS) {
-                numeroCaramelos = (int)(Math.random() * (MAX_CARAMELOS_POR_CASA - MIN_CARAMELOS_POR_CASA + 1)) + MIN_CARAMELOS_POR_CASA;
-                caramelosNino3 += numeroCaramelos;
-                System.out.println("Niño 3 recibió " + numeroCaramelos + " caramelos");          
-                if(caramelosNino3 >= MAX_CARAMELOS) { 
-                  bolsaNino3 = true;
-                  caramelosNino3 = MAX_CARAMELOS;
-                  System.out.println("¡La bolsa del niño 3 está llena!"); 
-                }
-              }
+            for (int nino = 0; nino < 3; nino++) {
+                        caramelos[nino] = repartirCaramelos(
+                                caramelos[nino],
+                                bolsaLlena,
+                                nino,
+                                danCaramelos,
+                                P_CARAMELOS,
+                                MAX_CARAMELOS_POR_CASA,
+                                MIN_CARAMELOS_POR_CASA,
+                                MAX_CARAMELOS
+                        );
             }
           }
           else { 
@@ -78,16 +51,40 @@ public class Halloween {
           }
           inputUsuario = scanner.nextLine();
         }             
-      } while ((pisoActual <= MAX_PISOS) && !(bolsaNino1 && bolsaNino2  && bolsaNino3));
+      } while ((pisoActual <= MAX_PISOS) && !(bolsaLlena[0] && bolsaLlena[1] && bolsaLlena[2]));
 
-      if(bolsaNino1 && bolsaNino2 && bolsaNino3) { 
+      if (bolsaLlena[0] && bolsaLlena[1] && bolsaLlena[2]) { 
         System.out.println("\n¡Todas las bolsas están llenas! Terminamos la búsqueda.");
       }
 
       System.out.println("\n=== Resultados Finales ===");
       System.out.println("Casas visitadas: " + casasVisitadas );
-      System.out.println("Niño 1: " + caramelosNino1 + " caramelos" + (bolsaNino1 ? " (Bolsa llena)" : ""));
-      System.out.println("Niño 2: " + caramelosNino2 + " caramelos" + (bolsaNino2 ? " (Bolsa llena)" : ""));
-      System.out.println("Niño 3: " + caramelosNino3+ " caramelos" + (bolsaNino3 ? " (Bolsa llena)" : ""));
+        for (int i = 0; i < 3; i++) {
+                System.out.println("Niño " + (i + 1) + ": " + caramelos[i] + " caramelos" + (bolsaLlena[i] ? " (Bolsa llena)" : ""));
+        }
     }
+
+     static int repartirCaramelos(int caramelosActuales, boolean[] bolsaLlena, int indiceNino,
+                                 double danCaramelos, double P_CARAMELOS,
+                                 int MAX_CARAMELOS_POR_CASA, int MIN_CARAMELOS_POR_CASA,
+                                 int MAX_CARAMELOS) {
+
+        if (!bolsaLlena[indiceNino]) {
+            if (danCaramelos <= P_CARAMELOS) {
+                int numeroCaramelos = (int) (Math.random() * (MAX_CARAMELOS_POR_CASA - MIN_CARAMELOS_POR_CASA + 1)) + MIN_CARAMELOS_POR_CASA;
+
+                caramelosActuales += numeroCaramelos;
+                System.out.println("Niño " + (indiceNino + 1) + " recibió " + numeroCaramelos + " caramelos");
+
+                if (caramelosActuales >= MAX_CARAMELOS) {
+                    bolsaLlena[indiceNino] = true;
+                    caramelosActuales = MAX_CARAMELOS;
+                    System.out.println("¡La bolsa del niño " + (indiceNino + 1) + " está llena!");
+                }
+            }
+        }
+
+        return caramelosActuales;
+    }
+
 }
